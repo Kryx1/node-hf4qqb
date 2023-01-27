9712567e2cefcc5ec19556ddf840532b2e5de8b0
Kryx1 <122876831+Kryx1@users.noreply.github.com>
2023-01-27 08:38:32 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-hf4qqb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-hf4qqb)
---
+++ index.js
@@ -0,0 +1,4 @@
+git clone https://github.com/amethystnetwork-dev/Tsunami
+cd Tsunami
+npm install
+npm start
